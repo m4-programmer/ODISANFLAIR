34196354c4569fc8b5bf5a061aff97b1699c31dc
Adolphus Miracle <miraboy13@gmail.com>
2023-07-29 14:32:54 +0100

    created controller to manage posts

--- resources/views/layouts/admin_sidebar.blade.php
+++ resources/views/layouts/admin_sidebar.blade.php
@@ -43,6 +43,15 @@
                     </a>
                 </li>
                 <li class="nav-item">
+                    <a href="{{route('admin.categories.index')}}" class="nav-link">
+                        <i class="nav-icon fas fa-th"></i>
+                        <p>
+                            Categories
+                            <span class="right badge badge-danger">New</span>
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
                     <a href="#" class="nav-link">
                         <i class="nav-icon fas fa-copy"></i>
                         <p>
