3aa47f0f98226cded85037f8719de86cb9931ad9
Adolphus Miracle <miraboy13@gmail.com>
2024-08-17 13:14:14 +0100

    feat: created enum for tag

---
+++ app/Enums/TagEnum.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+enum TagEnum: string
+{
+    case FOREX_TRADING = "forex trading";
+    case CRYPTO_TRADING = "crypto trading";
+    case STOCKS_TRADING = "stocks trading";
+    case BUSINESS_ACUMEN = "business acumen";
+}
