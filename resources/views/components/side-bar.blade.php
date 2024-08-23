30b3120aa913c1f2822334d54c26d2fa16a680c6
Adolphus Miracle <miraboy13@gmail.com>
2024-08-24 00:43:01 +0100

    chore: refactored component

--- resources/views/components/side-bar.blade.php
+++ resources/views/components/side-bar.blade.php
@@ -4,13 +4,13 @@
         <div class="aside-body">
             <div class="featured-author">
                 <div class="featured-author-inner">
-                    <div class="featured-author-cover" style="background-image: url({{asset('asset/images/news/img15.jpg')}});">
+                    <div class="featured-author-cover" style="background-image: url({{asset('asset/images/news/img12.jpg')}});">
                         <div class="badges">
                             <div class="badge-item"><i class="ion-star"></i> Founder</div>
                         </div>
                         <div class="featured-author-center">
                             <figure class="featured-author-picture">
-                                <img src="{{asset('asset/images/img01.jpg')}}" alt="Founder">
+                                <img src="{{asset('images-portfolio/MyPhoto.png')}}" alt="Founder">
                             </figure>
                             <div class="featured-author-info">
                                 <h2 class="name">{{$author->name}}</h2>
@@ -48,15 +48,15 @@
                             <h2 class="block-title">Photos</h2>
                             <div class="block-body">
                                 <ul class="item-list-round" data-magnific="gallery">
-                                    <li><a href="{{asset('asset/images/news/img06.jpg')}}" style="background-image: url('{{asset('asset/images/news/img06.jpg')}}');"></a></li>
-                                    <li><a href="{{asset('asset/images/news/img07.jpg')}}" style="background-image: url('{{asset('asset/images/news/img07.jpg')}}');"></a></li>
-                                    <li><a href="{{asset('asset/images/news/img08.jpg')}}" style="background-image: url('{{asset('asset/images/news/img08.jpg')}}');"></a></li>
-                                    <li><a href="{{asset('asset/images/news/img09.jpg')}}" style="background-image: url('{{asset('asset/images/news/img09.jpg')}}');"></a></li>
-                                    <li><a href="{{asset('asset/images/news/img10.jpg')}}" style="background-image: url('{{asset('asset/images/news/img10.jpg')}}');"></a></li>
-                                    <li><a href="{{asset('asset/images/news/img11.jpg')}}" style="background-image: url('{{asset('asset/images/news/img11.jpg')}}');"></a></li>
-                                    <li><a href="{{asset('asset/images/news/img12.jpg')}}" style="background-image: url('{{asset('asset/images/news/img12.jpg')}}');"><div class="more">+2</div></a></li>
-                                    <li class="hidden"><a href="{{asset('asset/images/news/img13.jpg')}}" style="background-image: url('{{asset('asset/images/news/img13.jpg')}}');"></a></li>
-                                    <li class="hidden"><a href="{{asset('asset/images/news/img14.jpg')}}" style="background-image: url('{{asset('asset/images/news/img14.jpg')}}');"></a></li>
+                                    <li><a href="{{asset('images-portfolio/folio/Picture10.png')}}" style="background-image: url('{{asset('images-portfolio/folio/Picture10.png')}}');"></a></li>
+                                    <li><a href="{{asset('images-portfolio/folio/Piture5.png')}}" style="background-image: url('{{asset('images-portfolio/folio/Piture5.png')}}');"></a></li>
+                                    <li><a href="{{asset('images-portfolio/folio/Piture7.png')}}" style="background-image: url('{{asset('images-portfolio/folio/Piture7.png')}}');"></a></li>
+                                    <li><a href="{{asset('images-portfolio/folio/Piture1.png')}}" style="background-image: url('{{asset('images-portfolio/folio/Piture1.png')}}');"></a></li>
+                                    <li><a href="{{asset('images-portfolio/folio/Picture12.png')}}" style="background-image: url('{{asset('images-portfolio/folio/Picture12.png')}}');"></a></li>
+                                    <li><a href="{{asset('images-portfolio/folio/Picture9.png')}}" style="background-image: url('{{asset('images-portfolio/folio/Picture9.png')}}');"></a></li>
+                                    <li><a href="{{asset('images-portfolio/folio/Piture4.png')}}" style="background-image: url('{{asset('images-portfolio/folio/Piture4.png')}}');"><div class="more">+2</div></a></li>
+                                    <li class="hidden"><a href="{{asset('images-portfolio/folio/Piture6.png')}}" style="background-image: url('{{asset('images-portfolio/folio/Piture6.png')}}');"></a></li>
+                                    <li class="hidden"><a href="{{asset('images-portfolio/folio/Piture2.png')}}" style="background-image: url('{{asset('images-portfolio/folio/Piture2.png')}}');"></a></li>
                                 </ul>
                             </div>
                         </div>
