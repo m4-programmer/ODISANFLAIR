fc60bf28a09b2888bf270adc6dfa220e8cc60f57
Adolphus Miracle <miraboy13@gmail.com>
2024-11-02 19:46:35 +0100

    feat: implemented visitors traffic logic

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -6,16 +6,26 @@ use App\Http\Controllers\Controller;
 use App\Models\Comment;
 use App\Models\Post;
 use App\Models\Tag;
+use App\Service\WebsiteTrafficService;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
+    public function __construct(public WebsiteTrafficService $traffic){
+
+    }
+
     public function index(){
         $totalCategories = Tag::count();
         $totalPosts = Post::count();
         $totalComments = Comment::count();
-        $totalUniqueVisitors = 100;
         $totalLikes = 150;
-        return view('admin.dashboard',compact('totalCategories','totalPosts','totalComments','totalUniqueVisitors','totalLikes'));
+
+        $totalUniqueVisitors = $this->traffic->getAllUniqueVisitors();
+        $todayVisitors = $this->traffic->getTodayUniqueVisitors();
+        $mostVisitedPage = $this->traffic->getMostVisitedPages();
+        $visitorsByDevice = $this->traffic->getVisitorByDeviceType();
+        return view('admin.dashboard',compact('totalCategories','totalPosts','totalComments','totalUniqueVisitors','totalLikes', 'todayVisitors','mostVisitedPage',
+            'visitorsByDevice'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,48 +12,50 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-//Admin Routes
-Route::prefix('admin')->name('admin.')->group(function (){
-    Route::get('/dashboard',[DashboardController::class,'index'])->name('dashboard');
-    Route::resource('/categories', ManageCategoriesAlias::class);
-    Route::resource('/posts', ManagePostController::class);
-    Route::resource('/videos', ManageVideosController::class);
-    Route::resource('/audios', ManageAudiosController::class);
-    Route::resource('/comments', ManageCommentController::class);
-});
-Route::get('/index', [WelcomeController::class,'index'])->name('blog_index');
-Route::get('/', [WelcomeController::class,'portfolio']);
-Route::get('/search', [WelcomeController::class,'search'])->name('search');
-Route::get('/category/latest',[CategoryController::class,'latest'])->name('latest');
-Route::get('/category/popular',[CategoryController::class,'popular'])->name('popular');
-Route::get('/category/{title}', [CategoryController::class,'index'])->name('category');
-Route::get("/videos", GetVideosController::class)->name("videos");
-Route::get('/{category}/{post_slug}', [PostController::class,'index']);
-Route::post('/{category}/{post_slug}', [PostController::class,'store']);
-Route::post('/newsletter',[App\Http\Controllers\Controller::class,'subcribe']);
-Route::get('/contact',function (){
-    return view('contact');
-});
-Route::get('/about',function (){
-    return view('about');
-});
-Route::get('/privacy-policy',function (){
-    return view('privacy');
-});
-Route::get('/disclaimer',function (){
-    return view('disclaimer');
-})->name('disclaimer');
-Route::get('/scam-alert',function (){
-    return view('scam_alert');
-})->name('scam_alert');
-Route::get('/starter',function (){
-    return view('starter');
-});
+Route::middleware("traffic")->group(function (){
+    //Admin Routes
+    Route::prefix('admin')->name('admin.')->group(function (){
+        Route::get('/dashboard',[DashboardController::class,'index'])->name('dashboard');
+        Route::resource('/categories', ManageCategoriesAlias::class);
+        Route::resource('/posts', ManagePostController::class);
+        Route::resource('/videos', ManageVideosController::class);
+        Route::resource('/audios', ManageAudiosController::class);
+        Route::resource('/comments', ManageCommentController::class);
+    });
+    Route::get('/index', [WelcomeController::class,'index'])->name('blog_index');
+    Route::get('/', [WelcomeController::class,'portfolio']);
+    Route::get('/search', [WelcomeController::class,'search'])->name('search');
+    Route::get('/category/latest',[CategoryController::class,'latest'])->name('latest');
+    Route::get('/category/popular',[CategoryController::class,'popular'])->name('popular');
+    Route::get('/category/{title}', [CategoryController::class,'index'])->name('category');
+    Route::get("/videos", GetVideosController::class)->name("videos");
+    Route::get('/{category}/{post_slug}', [PostController::class,'index']);
+    Route::post('/{category}/{post_slug}', [PostController::class,'store']);
+    Route::post('/newsletter',[App\Http\Controllers\Controller::class,'subcribe']);
+    Route::get('/contact',function (){
+        return view('contact');
+    });
+    Route::get('/about',function (){
+        return view('about');
+    });
+    Route::get('/privacy-policy',function (){
+        return view('privacy');
+    });
+    Route::get('/disclaimer',function (){
+        return view('disclaimer');
+    })->name('disclaimer');
+    Route::get('/scam-alert',function (){
+        return view('scam_alert');
+    })->name('scam_alert');
+    Route::get('/starter',function (){
+        return view('starter');
+    });
 
-Auth::routes();
+    Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/{category}', [CategoryController::class,'dynamicContent'])->name('category_card');
+    Route::get('/{category}', [CategoryController::class,'dynamicContent'])->name('category_card');
+});
 
 
