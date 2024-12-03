98a289ea1f9e61b892e58efe8ce75ccd0c3bb7d6
Adolphus Miracle <miraboy13@gmail.com>
2024-12-03 21:37:23 +0100

    chore: refactored routes

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,15 @@
 <?php
 
-use App\Http\Controllers\Admin\DashboardController;
-use App\Http\Controllers\Admin\ManageCategories as ManageCategoriesAlias;
 use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\GetVideosController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ManageAudiosController;
-use App\Http\Controllers\ManageCommentController;
-use App\Http\Controllers\ManagePostController;
-use App\Http\Controllers\ManageVideosController;
 use App\Http\Controllers\PostController;
-use App\Http\Controllers\ViewVideoController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware("traffic")->group(function (){
     //Admin Routes
-    Route::prefix('admin')->name('admin.')->group(function (){
-        Route::get('/dashboard',[DashboardController::class,'index'])->name('dashboard')->withoutMiddleware('traffic');
-        Route::resource('/categories', ManageCategoriesAlias::class);
-        Route::resource('/posts', ManagePostController::class);
-        Route::resource('/videos', ManageVideosController::class);
-        Route::resource('/audios', ManageAudiosController::class);
-        Route::resource('/comments', ManageCommentController::class);
-    });
+    Route::prefix('admin')->name('admin.')->group(base_path('routes/admin.php'));
+
     Route::get('/index', [WelcomeController::class,'index'])->name('blog_index');
     Route::get('/', [WelcomeController::class,'portfolio']);
     Route::get('/search', [WelcomeController::class,'search'])->name('search');
@@ -38,21 +24,12 @@ Route::middleware("traffic")->group(function (){
     Route::get('/contact',function (){
         return view('contact');
     });
-    Route::get('/about',function (){
-        return view('about');
-    });
-    Route::get('/privacy-policy',function (){
-        return view('privacy');
-    });
-    Route::get('/disclaimer',function (){
-        return view('disclaimer');
-    })->name('disclaimer');
-    Route::get('/scam-alert',function (){
-        return view('scam_alert');
-    })->name('scam_alert');
-    Route::get('/starter',function (){
-        return view('starter');
-    });
+    Route::view('/about', 'about');
+    Route::view('/privacy-policy', 'privacy');
+
+    Route::view('/disclaimer', 'disclaimer')->name('disclaimer');
+    Route::view('/scam-alert', 'scam_alert')->name('scam_alert');
+    Route::view('/starter', 'starter');
 
     Auth::routes();
 
