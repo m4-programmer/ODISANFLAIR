23d01efc5d9b80a551de9babbe8a4379903afa57
Daniel Somto <flaremarkets@gmail.com>
2025-04-14 04:56:31 +0100

    checking banners from monetag

--- resources/views/components/html-head.blade.php
+++ resources/views/components/html-head.blade.php
@@ -33,12 +33,17 @@
     <!-- my adsence from Monetag -->
     <meta name="monetag" content="c63a7a7b57e6dedc3acd6e81a4a33ee5">
      <!-- adsence 1 -->
-    <script type="text/javascript">  atOptions = {   'key' : 'b89299cc542e36bf5d734c0053c35ec8',   'format' : 'iframe',   'height' : 60,   'width' : 468,   'params' : {}  }; </script> <script type="text/javascript" src="//www.highperformanceformat.com/b89299cc542e36bf5d734c0053c35ec8/invoke.js"></script>
-    <!-- adsence 2 -->
-    <script type="text/javascript">  atOptions = {   'key' : '053966c4568854eaaf58e5b5dc1e2563',   'format' : 'iframe',   'height' : 300,   'width' : 160,   'params' : {}  }; </script> <script type="text/javascript" src="//www.highperformanceformat.com/053966c4568854eaaf58e5b5dc1e2563/invoke.js"></script>
-    <!-- adsence 3 -->
-    <script type="text/javascript">  atOptions = {   'key' : 'e8d0deef1b18720055b87279d3088e7a',   'format' : 'iframe',   'height' : 600,   'width' : 160,   'params' : {}  }; </script> <script type="text/javascript" src="//www.highperformanceformat.com/e8d0deef1b18720055b87279d3088e7a/invoke.js"></script> 
-    <!-- for monetag adsence -->
-    <script src="https://kulroakonsu.net/88/tag.min.js" data-zone="138936" async data-cfasync="false"></script>
-    <script src="https://kulroakonsu.net/88/tag.min.js" data-zone="142139" async data-cfasync="false"></script>
+     <!-- <script type="text/javascript">  atOptions = {   'key' : 'b89299cc542e36bf5d734c0053c35ec8',   'format' : 'iframe',   'height' : 60,   'width' : 468,   'params' : {}  }; </script> <script type="text/javascript" src="//www.highperformanceformat.com/b89299cc542e36bf5d734c0053c35ec8/invoke.js"></script>
+     -->
+     <!-- adsence 2 -->
+     <!-- <script type="text/javascript">  atOptions = {   'key' : '053966c4568854eaaf58e5b5dc1e2563',   'format' : 'iframe',   'height' : 300,   'width' : 160,   'params' : {}  }; </script> <script type="text/javascript" src="//www.highperformanceformat.com/053966c4568854eaaf58e5b5dc1e2563/invoke.js"></script>
+     -->
+     <!-- adsence 3 -->
+    
+     <!-- <script type="text/javascript">  atOptions = {   'key' : 'e8d0deef1b18720055b87279d3088e7a',   'format' : 'iframe',   'height' : 600,   'width' : 160,   'params' : {}  }; </script> <script type="text/javascript" src="//www.highperformanceformat.com/e8d0deef1b18720055b87279d3088e7a/invoke.js"></script>  -->
+
+    <!--in push (banner ads) -->
+    <script>(function(d,z,s){s.src='https://'+d+'/400/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('vemtoutcheeg.com',9209471,document.createElement('script'))</script>
+    <!-- native baner -->
+    <script>(function(d,z,s){s.src='https://'+d+'/401/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('groleegni.net',9209481,document.createElement('script'))</script> 
 </head>
